34032fcf9ec7534a9c5bf1849e60efcd2d04f6b8
Avish Shah <avishshah1108@gmail.com>
2020-07-15 20:37:13 +0530

    Add files via upload

---
+++ youtube_dl.py
@@ -0,0 +1,16 @@
+# Author: Avish Shah
+# Date and Time of creation: 15 July 2020, 8:27 PM
+
+
+from pytube import YouTube
+
+url = input("Please input the link: ")
+yt = YouTube(url)
+stream = yt.streams.filter(progressive = True)
+print(*stream, sep='\n')
+print('\n'"Above are the available itag(s) from which you can download the video.")
+itag = int(input('\n'"Please enter the itag value from the above list: "))
+folder = input('\n'"Please enter the folder name were you want to save video: ") # Skip this if you want to save it in current directory. 
+dnld = yt.streams.get_by_itag(itag).download(folder) 
+print(dnld)
+print('\n'"Yep! Your Video is downloaded")
